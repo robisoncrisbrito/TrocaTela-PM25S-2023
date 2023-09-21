5dceb8d1b40a403a2effc648f8b6d57383301ca9
robisoncrisbrito <robison.brito@gmail.com>
2023-09-20 21:59:53 -0300

    com duas telas

---
+++ app/src/main/java/br/edu/utfpr/trocatela/ConfirmarActivity.kt
@@ -0,0 +1,15 @@
+package br.edu.utfpr.trocatela
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class ConfirmarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmar)
+    }
+
+    fun btListarOnClick(view: View) {}
+    fun btConfirmarOnClick(view: View) {}
+}
