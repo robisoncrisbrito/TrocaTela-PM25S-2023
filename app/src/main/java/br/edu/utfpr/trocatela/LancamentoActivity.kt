8d55d32d6fc0bcf97e923be72a9b27dbc2c9ab67
robisoncrisbrito <robison.brito@gmail.com>
2023-09-20 22:05:03 -0300

    com duas telas

--- app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
@@ -3,11 +3,19 @@ package br.edu.utfpr.trocatela
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class LancamentoActivity : AppCompatActivity() {
+
+    private lateinit var etCod : EditText
+    private lateinit var etQtd : EditText
+    private lateinit var etValor : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lancamento)
+
+        etCod = findViewById( )
     }
 
     fun btConfirmarOnClick(view: View) {
